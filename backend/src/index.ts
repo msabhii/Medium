7d575fa2c4cf9d3feb23a1632d0a33ff6dad7b80
msabhii <msabhithakur7777@gmail.com>
2024-09-29 21:57:29 +0530

    Added signUp 29-09-2024

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from "hono";
 import mainRouter from "./routes/index";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 const app = new Hono();
 
 app.route("/", mainRouter);
--- backend/src/routes/User.ts
+++ backend/src/routes/User.ts
@@ -1,8 +1,38 @@
 import { Hono } from "hono";
-const routerUser = new Hono();
+import { PrismaClient } from "@prisma/client/edge";
+import { decode, sign, verify } from "hono/jwt";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { use } from "hono/jsx";
 
-routerUser.post("/api/v1/user/signup", (c) => {
-  return c.text("Helo");
+const routerUser = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWTSECRET: string;
+  };
+}>();
+
+routerUser.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+
+  const payload = {
+    id: user.id,
+  };
+  const sercret = c.env.JWTSECRET;
+  const token = await sign(payload, sercret);
+
+  return c.json({
+    jwt: token,
+  });
 });
 
 routerUser.post("/api/v1/user/signin", (c) => {
