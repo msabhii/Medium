fdae434c4a53500f4aedee7a785b8efda3619aed
msabhii <msabhithakur7777@gmail.com>
2024-10-10 22:09:57 +0530

    Done with Backend and pushed to npm

---
+++ common/node_modules/@mscode07/mscodeblog/dist/zod.d.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+export declare const signUpInput: z.ZodObject<{
+    username: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    username: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    username: string;
+    password: string;
+    name?: string | undefined;
+}>;
